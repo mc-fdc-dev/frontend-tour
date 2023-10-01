ae5af1e17ff449121fb74dc4cc53a658061811a4
mc_fdc <Masato.04.11.2007@gmail.com>
2023-10-01 14:24:50 +0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function qSel(selectors) {
+    return document.querySelector(selectors);
+}
+;
+var nfe = qSel("header nav.navigation-fixed");
+function scrollEndHandler() {
+    if (scrollY < 500) {
+        nfe.classList.add("hide");
+    }
+    else {
+        nfe.classList.remove("hide");
+    }
+    ;
+}
+;
+scrollEndHandler();
+addEventListener("scroll", scrollEndHandler);
